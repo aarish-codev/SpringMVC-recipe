9ddafda5c896f94d1c764fcaaca9d72e2c09d67c
Aarish Saxena <aarish.work@gmail.com>
2018-05-18 17:48:30 +0530

    added sql data

--- src/main/java/com/springmvc/recipe/domain/Category.java
+++ src/main/java/com/springmvc/recipe/domain/Category.java
@@ -10,7 +10,31 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String descripton;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripton() {
+        return description;
+    }
+
+    public void setDescripton(String descripton) {
+        this.description = descripton;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
